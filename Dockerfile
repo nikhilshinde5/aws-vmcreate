e3d36437fb97c644422a2cdb152d11f6811d90f1
Ubuntu <ubuntu@ip-172-31-93-74.ec2.internal>
2023-02-21 05:09:02 +0000

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM registry.access.redhat.com/ubi8/go-toolset:1.18.4-8.1669838000
+FROM registry.access.redhat.com/ubi8/ubi-minimal
 
 # Labels
 LABEL name="aws-vmcreate" \
-    maintainer="xyzcompany.com" \
-    vendor="xyzcompany" \
+    maintainer="nikhil.com" \
+    vendor="nikhil" \
     version="1.0.0" \
     release="1" \
     summary="This service enables provisioning/de-provisioning of AWS cloud vms." \
@@ -16,13 +16,15 @@ RUN chgrp -R 0 /opt && \
     chmod -R g=u /opt && \
     chmod +x -R /opt
 USER 1001
-ENV ec2_tag_key "POC"
-ENV ec2_tag_value "GolangOperator"
+ENV ec2_tag_key "Name"
+ENV ec2_tag_value "nikhil-test"
 ENV ec2_command "create"
+# ENV ec2_image_id "ami-0d0ca2066b861631c"
+# ENV ec2_instance_type "t2.micro"
 #can be delete also
-
-COPY go.* ./
-COPY aws-vmcreate.go .
-RUN go mod download
-RUN go build -o aws-vmcreate
-CMD ["bash","-c","/opt/aws-vmcreate -c  $ec2_command -n $ec2_tag_key -v $ec2_tag_value "]
+# COPY go.* ./
+COPY aws-vmcreate .
+# ADD data data/
+# RUN go mod download
+# RUN go build -o aws-vmcreate
+CMD ["bash","-c","/opt/aws-vmcreate -c  $ec2_command -n $ec2_tag_key -v $ec2_tag_value"]
--- aws-vmcreate.go
+++ aws-vmcreate.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"os"
+	"strings"
 
 	"fmt"
 
@@ -24,6 +27,22 @@ type EC2CreateInstanceAPI interface {
 	CreateTags(ctx context.Context,
 		params *ec2.CreateTagsInput,
 		optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+
+	TerminateInstances(ctx context.Context,
+		params *ec2.TerminateInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.TerminateInstancesOutput, error)
+
+	// ModifyInstanceAttribute(ctx context.Context,
+	// 	params *ec2.ModifyInstanceAttributeInput,
+	// 	optFns ...func(*ec2.Options)) (*ec2.ModifyInstanceAttributeOutput, error)
+	// StopInstances(ctx context.Context,
+	// 	params *ec2.StopInstancesInput,
+	// 	optFns ...func(*ec2.Options)) (*ec2.StopInstancesOutput, error)
+}
+
+type ConfigMap struct {
+	InstanceType string `json:"instance_type"`
+	ImageId      string `json:"image_id"`
 }
 
 // MakeInstance creates an Amazon Elastic Compute Cloud (Amazon EC2) instance.
@@ -55,17 +74,96 @@ func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunIns
 func MakeTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
 	return api.CreateTags(c, input)
 }
-func DeleteInstancesCmd() {
-	fmt.Println("TO DO")
+
+// DeleteInstance deletes an Amazon Elastic Compute Cloud (Amazon EC2) instance.
+// Inputs:
+//
+//	c is the context of the method call, which includes the AWS Region.
+//	api is the interface that defines the method call.
+//	input defines the input arguments to the service call.
+//
+// Output:
+//
+//	If success, a TerminateInstancesInput object containing the result of the service call and nil.
+//	Otherwise, nil and an error from the call to TerminateInstances.
+func DeleteInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	return api.TerminateInstances(c, input)
+}
+
+// func UpdateInstanceAttribute(c context.Context, api EC2CreateInstanceAPI, input *ec2.ModifyInstanceAttributeInput) (*ec2.ModifyInstanceAttributeOutput, error) {
+// 	return api.ModifyInstanceAttribute(c, input)
+// }
+
+// func PauseInstances(c context.Context, api EC2CreateInstanceAPI, input *ec2.StopInstancesInput) (*ec2.StopInstancesOutput, error) {
+// 	return api.StopInstances(c, input)
+// }
+
+func DeleteInstancesCmd(name *string, value *string) {
+
+	var instanceIds = make([]string, 0)
+
+	val := strings.Split(*value, ",")
+	tag := "tag:" + *name
+
+	describeInput := &ec2.DescribeInstancesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String(tag),
+				Values: val,
+			},
+		},
+	}
+	result, err := client.DescribeInstances(context.TODO(), describeInput)
+	if err != nil {
+		fmt.Println("Got an error fetching the status of the instance")
+		fmt.Println(err)
+	} else {
+		for _, r := range result.Reservations {
+			fmt.Println("Instance IDs:")
+			for _, i := range r.Instances {
+				instanceIds = append(instanceIds, *i.InstanceId)
+			}
+			fmt.Println(instanceIds)
+		}
+
+		input := &ec2.TerminateInstancesInput{
+			InstanceIds: instanceIds,
+			DryRun:      new(bool),
+		}
+
+		result, err := DeleteInstance(context.TODO(), client, input)
+		if err != nil {
+			fmt.Println("Got an error terminating the instance:")
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Terminated instance with id: ", *result.TerminatingInstances[0].InstanceId)
+	}
 }
 
 func CreateInstancesCmd(name *string, value *string) {
 	// Create separate values if required.
 	minMaxCount := int32(1)
 
+	file, err := os.Open("data/config.json")
+	if err != nil {
+		fmt.Println("Error opening config file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var config ConfigMap
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		fmt.Println("Error decoding config:", err)
+		os.Exit(1)
+	}
+
+	// instanceType := &config.InstanceType
+
 	input := &ec2.RunInstancesInput{
-		ImageId:      aws.String("ami-0d0ca2066b861631c"),
-		InstanceType: types.InstanceTypeT2Micro,
+		ImageId:      aws.String(config.ImageId),
+		InstanceType: (types.InstanceType)(config.InstanceType),
 		MinCount:     &minMaxCount,
 		MaxCount:     &minMaxCount,
 	}
@@ -95,6 +193,65 @@ func CreateInstancesCmd(name *string, value *string) {
 	}
 
 	fmt.Println("Created tagged instance with ID " + *result.Instances[0].InstanceId)
+
+	//Testing change of instanceType
+	// fmt.Println("Updating instance type of instance with ID " + *result.Instances[0].InstanceId)
+	// time.Sleep(30 * time.Second)
+
+	// instanceID := *result.Instances[0].InstanceId
+	// newInstanceType := "t2.nano"
+
+	// //Stopping instances before changing instance type
+	// fmt.Println("Stopping instances before changing instance type")
+	// stopInstancesInput := &ec2.StopInstancesInput{
+	// 	InstanceIds: []string{instanceID},
+	// 	Force:       aws.Bool(false),
+	// }
+	// _, err = PauseInstances(context.TODO(), client, stopInstancesInput)
+	// if err != nil {
+	// 	fmt.Println("Got an error stoping the instance:")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// //this sleep for letting ec2 stop
+	// time.Sleep(60 * time.Second)
+	// Wait for the instance to be stopped
+	// for {
+	// 	describeOutput, err := svc.DescribeInstances(context.Background(), &ec2.DescribeInstancesInput{
+	// 		InstanceIds: []string{instanceID},
+	// 	})
+	// 	if err != nil {
+	// 		fmt.Println("Error describing instance:", err)
+	// 		return
+	// 	}
+	// 	if describeOutput.Reservations[0].Instances[0].State.Name == ec2.InstanceStateNameStopped {
+	// 		break
+	// 	}
+	// 	time.Sleep(5 * time.Second)
+	// }
+
+	// Modify the instance type
+	// _, err = svc.ModifyInstanceAttribute(context.Background(), &ec2.ModifyInstanceAttributeInput{
+	// 	InstanceId: &instanceID,
+	// 	InstanceType: &ec2.AttributeValue{
+	// 		Value: &newInstanceType,
+	// 	},
+
+	// attributeInput := &ec2.ModifyInstanceAttributeInput{
+	// 	InstanceId: &instanceID,
+	// 	InstanceType: &types.AttributeValue{
+	// 		Value: &newInstanceType,
+	// 	},
+	// }
+
+	// _, err = UpdateInstanceAttribute(context.TODO(), client, attributeInput)
+	// if err != nil {
+	// 	fmt.Println("Got an error updating the instance:")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
 }
 func init() {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
@@ -109,6 +266,8 @@ func main() {
 	command := flag.String("c", "", "command  create or delete")
 	name := flag.String("n", "", "The name of the tag to attach to the instance")
 	value := flag.String("v", "", "The value of the tag to attach to the instance")
+	// imageId := flag.String("i", "", "The instance id of the instance")
+	// instanceTypeString := flag.String("t", "", "The type of the instance")
 
 	flag.Parse()
 
@@ -127,6 +286,6 @@ func main() {
 	}
 
 	if *command == "delete" {
-		DeleteInstancesCmd()
+		DeleteInstancesCmd(name, value)
 	}
 }
